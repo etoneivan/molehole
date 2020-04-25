805ad8b7935d5d16b27f338506a3790b837e9325
etoneivan <64328374+etoneivan@users.noreply.github.com>
2020-04-25 22:07:34 +0300

    Add files via upload

---
+++ precache-manifest.f46eb71f148963c6509f77bf108e1307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e12dfd62e7381226cadb",
+    "url": "/static/css/main.282b161c.chunk.css"
+  },
+  {
+    "revision": "e12dfd62e7381226cadb",
+    "url": "/static/js/main.4161cf50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae1cd94de96c1563aaac",
+    "url": "/static/css/2.a8e5b4b9.chunk.css"
+  },
+  {
+    "revision": "ae1cd94de96c1563aaac",
+    "url": "/static/js/2.92d155d2.chunk.js"
+  },
+  {
+    "revision": "c4324d493babb2441a8865ed02068234",
+    "url": "/index.html"
+  }
+];
