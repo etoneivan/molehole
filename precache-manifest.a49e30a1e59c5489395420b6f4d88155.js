a1c631d1f1ff93011909a15ee7dfb7ff6b9675bd
etoneivan <64328374+etoneivan@users.noreply.github.com>
2020-04-26 18:56:08 +0300

    Add files via upload

---
+++ precache-manifest.a49e30a1e59c5489395420b6f4d88155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb5d1d7218d88a0d2290",
+    "url": "/molehole/static/css/main.282b161c.chunk.css"
+  },
+  {
+    "revision": "bb5d1d7218d88a0d2290",
+    "url": "/molehole/static/js/main.5cdebe6e.chunk.js"
+  },
+  {
+    "revision": "c2d224c50f46f469af07",
+    "url": "/molehole/static/js/runtime~main.03e63630.js"
+  },
+  {
+    "revision": "b55261bbab2eb4541bed",
+    "url": "/molehole/static/css/2.a8e5b4b9.chunk.css"
+  },
+  {
+    "revision": "b55261bbab2eb4541bed",
+    "url": "/molehole/static/js/2.ebec92aa.chunk.js"
+  },
+  {
+    "revision": "b13981c8494581c6178bc69c99009b44",
+    "url": "/molehole/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/molehole/precache-manifest.14f41eac1233934fca07206f77f1e8a4.js"
+  "/molehole/precache-manifest.a49e30a1e59c5489395420b6f4d88155.js"
 );
 
 workbox.clientsClaim();
